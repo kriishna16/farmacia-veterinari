21768506ffade1c27ed0a6b9f7807ade0d63a431
“kriishna16” <krishna.oteiza.navarro@gmail.com>
2024-09-10 09:16:10 -0300

    categoria veterinaria

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/veterinaria')
+def veterinaria():
+    return render_template('veterinaria.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
