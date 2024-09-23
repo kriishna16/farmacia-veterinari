3db4225a6bb84c57425dd6cfc19f234623f3e9e0
“kriishna16” <krishna.oteiza.navarro@gmail.com>
2024-09-23 09:14:41 -0300

    2

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ def caballos():
 @app.route('/herraje')
 def herraje():
     return render_template('herraje.html')
+
+@app.route('/corrales')
+def corrales():
+    return render_template('corrales.html')
 if __name__ == '__main__':
     
     app.run(debug=True)
